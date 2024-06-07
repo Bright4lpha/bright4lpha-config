321c6ec11bd011d7cbd16b690910c82e10fb3da1
HENRION Mathilde <mathilde.henrion@etu.univ-littoral.fr>
2024-06-07 14:44:43 +0200

    chore: add commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,20 @@
 export default {
     extends: ["@commitlint/config-angular"],
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            [
+                "feat",
+                "fix",
+                "docs",
+                "style",
+                "refactor",
+                "perf",
+                "test",
+                "chore",
+                "revert",
+            ],
+        ],
+    },
 };
